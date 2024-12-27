5cba4130e52eb92ac7d4ea63f9acf5d4e4f74409
Gabriel Victor da Silva de Souza <victorgabriel1730@gmail.com>
2024-12-27 16:15:23 -0300

    jest init

--- src/application/cadastrar-usuario.usecase.js
+++ src/application/cadastrar-usuario.usecase.js
@@ -3,7 +3,11 @@
 export function cadastrarUsuarioUseCase() { 
     return async function ({ nome_completo, CPF, telefone, endereco, email }) { 
         await usuariosRepository.cadastrar({
-            nome_completo, CPF, telefone, endereco, email
+            nome_completo,
+            CPF,
+            telefone,
+            endereco,
+            email
         })
     }
 }
